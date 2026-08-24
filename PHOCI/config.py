60c6c013e60f8f7e824c93dbbc4dbd6ebdd07304
yunyiwu <33896801+yunyiwu@users.noreply.github.com>
2026-08-24 10:46:30 +0800

    Update config.py

--- PHOCI/config.py
+++ PHOCI/config.py
@@ -43,10 +43,10 @@ config_train["chr_names"] = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "ch
 
 config_train["test_chr_names"] = ["chr13", "chr14", "chr15", "chrX", "chr22"]
 
-if train_cell_line = "GM12878":
+if train_cell_line == "GM12878":
     config_train["fc_ids"] = ["FC1", "FC2", "FC3", "FC4", "FC5", "FC6", "FC7"]
 
-if train_cell_line = "K562":
+if train_cell_line == "K562":
     config_train["fc_ids"] = ["FC1", "FC2", "FC3", "FC4"]
 
 ###########################set by users########################
